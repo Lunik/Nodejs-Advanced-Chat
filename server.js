9fe82a3e923ceb0b09434a792eb30ca5d79fdbcf
Lunik <guillaume.lunik@gmail.com>
2015-06-15 20:55:45 +0200

    plop

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 5000;
-var md5 = require('md5'); //crypt
+var md5 = require('MD5'); //crypt
 
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
